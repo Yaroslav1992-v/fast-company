5d12dceff793d07aa834adf630e24682905c7e4f
yaroslav1992-v <yaroslav1992russia@gmail.com>
2021-12-17 07:07:16 +0200

    chancged users

--- src/api/fake.api/professions.api.js
+++ src/api/fake.api/professions.api.js
@@ -4,7 +4,13 @@ export const professionsObject = {
     physics: { _id: "67rdca3eeb7f6fgeed471814", name: "Физик" },
     engineer: { _id: "67rdca3eeb7f6fgeed471822", name: "Инженер" },
     actor: { _id: "67rdca3eeb7f6fgeed471824", name: "Актер" },
-    cook: { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" }
+    cook: { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" },
+    cop: { _id: "67rdca3eeb6f6fgeed471829", name: "Полицейский" },
+    banker: { _id: "67rdca3eeb74rted471829", name: "Банкир" },
+    agent: { _id: "674dc53eeb7f6fgeed471829", name: "Спец-Анент" },
+    spy: { _id: "67rdca3ee27f6fgeed471829", name: "Шпион" },
+    queen: { _id: "67rdca3eeb7f6sveed471829", name: "Koролева" },
+    sales: { _id: "67rdca3eertsxsveed471829", name: "Продавец" }
 };
 export const professions = [
     { _id: "67rdca3eeb7f6fgeed471818", name: "Доктор" },
@@ -12,11 +18,17 @@ export const professions = [
     { _id: "67rdca3eeb7f6fgeed471814", name: "Физик" },
     { _id: "67rdca3eeb7f6fgeed471822", name: "Инженер" },
     { _id: "67rdca3eeb7f6fgeed471824", name: "Актер" },
-    { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" }
+    { _id: "67rdca3eeb7f6fgeed471829", name: "Повар" },
+    { _id: "67rdca3eeb6f6fgeed471829", name: "Полицейский" },
+    { _id: "67rdca3eeb74rted471829", name: "Банкир" },
+    { _id: "674dc53eeb7f6fgeed471829", name: "Спец-Анент" },
+    { _id: "67rdca3ee27f6fgeed471829", name: "Шпион" },
+    { _id: "67rdca3eeb7f6sveed471829", name: "Koролева" },
+    { _id: "67rdca3eertsxsveed471829", name: "Продавец" }
 ];
 const fetchAll = () =>
     new Promise((resolve) => {
-        window.setTimeout(function () {
+        window.setTimeout(function() {
             resolve(professionsObject);
         }, 2000);
     });
--- src/api/fake.api/user.api.js
+++ src/api/fake.api/user.api.js
@@ -29,7 +29,9 @@ const qualities = {
         _id: "67rdca3eeb7f6fgeed471103",
         name: "Неуверенный",
         color: "dark"
-    }
+    },
+    smart: { _id: "67rdca3eeb7f6fdee471102", name: "Умный", color: "info" },
+    dumb: { _id: "67rdca3esadwped471103", name: "Тупой", color: "dark" }
 };
 
 const users = [
@@ -39,8 +41,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.tedious, qualities.uncertain, qualities.strange],
         completedMeetings: 36,
-        rate: 2.5,
-        bookmark: false
+        rate: 2.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471816",
@@ -48,8 +49,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.buller, qualities.handsome, qualities.alcoholic],
         completedMeetings: 15,
-        rate: 2.5,
-        bookmark: false
+        rate: 2.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471817",
@@ -57,8 +57,7 @@ const users = [
         profession: professions.doctor,
         qualities: [qualities.buller],
         completedMeetings: 247,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471818",
@@ -66,8 +65,7 @@ const users = [
         profession: professions.waiter,
         qualities: [qualities.uncertain],
         completedMeetings: 148,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471819",
@@ -75,8 +73,7 @@ const users = [
         profession: professions.physics,
         qualities: [qualities.strange, qualities.tedious],
         completedMeetings: 37,
-        rate: 4.6,
-        bookmark: false
+        rate: 4.6
     },
     {
         _id: "67rdca3eeb7f6fgeed471820",
@@ -84,8 +81,7 @@ const users = [
         profession: professions.physics,
         qualities: [qualities.strange, qualities.uncertain],
         completedMeetings: 147,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471821",
@@ -93,8 +89,7 @@ const users = [
         profession: professions.engineer,
         qualities: [qualities.strange, qualities.tedious],
         completedMeetings: 72,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471822",
@@ -102,8 +97,7 @@ const users = [
         profession: professions.engineer,
         qualities: [qualities.handsome],
         completedMeetings: 72,
-        rate: 5,
-        bookmark: false
+        rate: 5
     },
     {
         _id: "67rdca3eeb7f6fgeed471823",
@@ -111,8 +105,7 @@ const users = [
         profession: professions.cook,
         qualities: [qualities.strange, qualities.uncertain],
         completedMeetings: 17,
-        rate: 4.5,
-        bookmark: false
+        rate: 4.5
     },
     {
         _id: "67rdca3eeb7f6fgeed471824",
@@ -120,8 +113,7 @@ const users = [
         profession: professions.cook,
         qualities: [qualities.handsome, qualities.buller],
         completedMeetings: 17,
-        rate: 4.5,
-        bookmark: false
+        rate: 4.5
     },
     {
         _id: "67rdca3eeb7f6fgeed47181f",
@@ -129,8 +121,7 @@ const users = [
         profession: professions.actor,
         qualities: [qualities.uncertain, qualities.strange],
         completedMeetings: 434,
-        rate: 3.5,
-        bookmark: false
+        rate: 3.5
     },
     {
         _id: "67rdca3eeb7f6fgeed47181r",
@@ -138,14 +129,101 @@ const users = [
         profession: professions.actor,
         qualities: [qualities.handsome],
         completedMeetings: 434,
-        rate: 5,
-        bookmark: false
+        rate: 5
+    },
+    {
+        _id: "67rdca3eeb7f6fgeed47112s",
+        name: "Декстер Морган",
+        profession: professions.cop,
+        qualities: [qualities.uncertain, qualities.strange],
+        completedMeetings: 90,
+        rate: 4
+    },
+    {
+        _id: "67rdcdsa7f6fgeed47112s",
+        name: "Джек Бауер",
+        profession: professions.agent,
+        qualities: [qualities.buller, qualities.handsome],
+        completedMeetings: 34,
+        rate: 4.5
+    },
+    {
+        _id: "67rdca3eeb7fasgeed47112s",
+        name: "Елизавета II",
+        profession: professions.queen,
+        qualities: [qualities.buller, qualities.alcoholic],
+        completedMeetings: 234,
+        rate: 4.5
+    },
+    {
+        _id: "67rdcaxxs7f6fgeed47112s",
+        name: "Джеймс Бонд",
+        profession: professions.spy,
+        qualities: [qualities.buller, qualities.handsome, qualities.alcoholic],
+        completedMeetings: 139,
+        rate: 3.5
+    },
+    {
+        _id: "67rxcaxxs8f6fgeed47112s",
+        name: "Тирион Ланнистер",
+        profession: professions.banker,
+        qualities: [qualities.buller, qualities.smart, qualities.alcoholic],
+        completedMeetings: 19,
+        rate: 5
+    },
+    {
+        _id: "67rxcaxxs8f6fgeedl7112p",
+        name: "Джейк Харпер",
+        profession: professions.cook,
+        qualities: [qualities.dumb, qualities.tedious],
+        completedMeetings: 89,
+        rate: 2
+    },
+    {
+        _id: "67rxcaxsad8f6fgeedl7112p",
+        name: "Уолтер Уайт",
+        profession: professions.cook,
+        qualities: [qualities.smart, qualities.buller],
+        completedMeetings: 28,
+        rate: 5
+    },
+    {
+        _id: "67rxcaxxs8s6fgeed47112s",
+        name: "Серсея Ланнистер",
+        profession: professions.queen,
+        qualities: [qualities.buller, qualities.alcoholic],
+        completedMeetings: 1239,
+        rate: 3
+    },
+    {
+        _id: "67rxcaxxs8faseed47112s",
+        name: "Майкл Скотт",
+        profession: professions.sales,
+        qualities: [qualities.dumb, qualities.tedious],
+        completedMeetings: 831,
+        rate: 3
+    },
+    {
+        _id: "67rxcasxs8faseed47112s",
+        name: "Майкл Скофилд",
+        profession: professions.engineer,
+        qualities: [qualities.smart, qualities.handsome],
+        completedMeetings: 239,
+        rate: 3.5
+    },
+    {
+        _id: "67rxcasxs8facdsa7112s",
+        name: "Рик Гримс",
+        profession: professions.cop,
+        qualities: [qualities.strange],
+        completedMeetings: 219,
+        rate: 3
     }
 ];
 
 const fetchAll = () =>
     new Promise((resolve) => {
-        window.setTimeout(function () {
+        window.setTimeout(function() {
             resolve(users);
         }, 2000);
     });
