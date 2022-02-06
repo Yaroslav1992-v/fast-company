dcdc272b589332be52caeb2228569916357cade7
Yaroslav1992-v <yaroslav1992russia@gmail.com>
2022-02-06 21:32:14 +0200

    added firebase database

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,9 +1,32 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import config from "../config.json";
-axios.defaults.baseURL = config.apiEndPoint;
+import configFile from "../config.json";
+axios.defaults.baseURL = configFile.apiEndPoint;
+axios.interceptors.request.use(
+    function (config) {
+        if (configFile.isFireBase) {
+            const containSlash = /\/$/gi.test(config.url);
+            config.url = (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
+        }
+
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    }
+);
+function transformData(data) {
+    return data ? Object.keys(data).map(key => ({
+        ...data[key]
+    })) : [];
+};
 axios.interceptors.response.use(
-    (res) => res,
+    (res) => {
+        if (configFile.isFireBase) {
+            res.data = { content: transformData(res.data) };
+            console.log(res.data);
+        }
+        return res;
+    },
     function (error) {
         const expectedErrors = error.response &&
             error.response.status >= 400
